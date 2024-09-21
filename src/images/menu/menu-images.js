04c3ec77240a8ea1f63f58947348642e8724a4ce
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-21 12:53:51 +0200

    Gather images for the menu page

---
+++ src/images/menu/menu-images.js
@@ -0,0 +1,6 @@
+export { default as figSalad } from './fig-salad.jpg';
+export { default as greenTea } from './green-tea.jpg';
+export { default as noodles } from './noodles.jpg';
+export { default as pizza } from './pizza.jpg';
+export { default as salad } from './salad.jpg';
+export { default as vegetables } from './vegetables.jpg';
---
+++ src/menu.js
@@ -0,0 +1,1 @@
+import * as menuImages from "./images/menu/menu-images";
