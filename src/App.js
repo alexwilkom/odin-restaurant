82193b7e4fd687448bca71d7dbf04a8bf3e974f6
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-21 17:34:35 +0200

    Rename button elements

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Contact from "./components/Contact";
 
 function App() {
     const contentDiv = document.querySelector("#content");
-    const homeDiv = document.querySelector("#btn-home");
-    const menuDiv = document.querySelector("#btn-menu");
-    const contactDiv = document.querySelector("#btn-contact");
+    const homeBtn = document.querySelector("#btn-home");
+    const menuBtn = document.querySelector("#btn-menu");
+    const contactBtn = document.querySelector("#btn-contact");
 
     const render = (component) => {
         contentDiv.innerHTML = "";
@@ -16,9 +16,9 @@ function App() {
     const initialize = () => {
         render(Home); // Render home on page load
 
-        homeDiv.addEventListener("click", () => render(Home));
-        menuDiv.addEventListener("click", () => render(Menu));
-        contactDiv.addEventListener("click", () => render(Contact));
+        homeBtn.addEventListener("click", () => render(Home));
+        menuBtn.addEventListener("click", () => render(Menu));
+        contactBtn.addEventListener("click", () => render(Contact));
     };
 
     document.addEventListener("DOMContentLoaded", initialize);
