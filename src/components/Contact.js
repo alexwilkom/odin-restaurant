f0556f3ff7c9a8d2f479a9b6aa9cb29b7ad44cf9
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-21 17:26:27 +0200

    Build contanct component

---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+function Contact() {
+    const contactSection = document.createElement("section");
+    contactSection.id = "contact";
+
+    const heading2 = document.createElement("h2");
+    heading2.innerHTML = "Get in touch with us";
+
+    const form = document.createElement("form");
+    form.innerHTML = `
+        <p>
+            <label for="client-name">Preferred name</label>
+            <input type="text" id="client-name" placeholder="Marcel Proust">
+        </p>
+        <p>
+            <label for="client-email">Email address</label>
+            <input type="email" id="client-email" placeholder="marcel@combray.com">
+        </p>
+        <p>
+            <label for="message">Message</label>
+            <textarea name="message" id="message"></textarea>
+        </p>
+        <button>Send</button>
+    `
+
+    contactSection.appendChild(heading2);
+    contactSection.appendChild(form);
+
+    return contactSection
+}
+
+export default Contact;
