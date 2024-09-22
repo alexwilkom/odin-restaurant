74f28a48acfb28729e69512e544873759d1c2098
Alex Wilkom <alex.wilkom@icloud.com>
2024-09-22 18:26:01 +0200

    Extract css into its own file

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: "./src/index.js",
@@ -16,12 +17,15 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
